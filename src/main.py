ad6986a4a56798b077a8f973852a5a9e1b523538
BruzaTom <thomasagomez77@gmail.com>
2024-09-19 04:02:40 -0500

    tree from path

--- src/main.py
+++ src/main.py
@@ -4,28 +4,58 @@ import shutil
 
 def main():
     path = 'static'
-    print(get_dir(path))
+    print({'public': get_dir(path)},'\n')
+    print(get_list(path))
 
-def update_dir(path, new):
+
+
+def update_dir(path, tree):
     if os.path.exists(path):
         shutil.rmtree(path)
+    for node in tree:
+        if tree[node] != None:
+            os.mkdir(node)
     
 
 def get_dir(path):
-    fileLst = []
     dir = {}
     if os.path.exists(path):
         dirLst = os.listdir(path)
         for node in dirLst:
-            if os.path.isfile(os.path.join(path, node)):
-                fileLst.append(os.path.join(path, node))
+            node_path = os.path.join(path, node)
+            if os.path.isfile(node_path):
+                dir[node_path.split('/')[-1]] = None
             else:
-                dir[os.path.join(path, node)] = get_dir(os.path.join(path, node))
-        if dir != {}:
-            dir[path] = fileLst
-            return dir
-        return fileLst
+                dir[node_path.split('/')[-1]] = get_dir(node_path)
+        return dir
     raise ValueError(f'{path} does not exist')
+
+
+def get_list(path):
+    list = []
+
+    def copy_to(path):
+        list = []
+        dict = {}
+        if os.path.isfile(path):
+            return [path]
+        copy = os.listdir(path)
+        for node in copy:
+            if os.path.isfile(os.path.join(path, node)):
+                list.append(os.path.join(path, node))
+            else:
+                list.extend(copy_to(os.path.join(path, node)))
+        return list
+
+    if os.path.exists(path):
+        copy = os.listdir(path)
+        for node in copy:
+            list.append(copy_to(os.path.join(path, node)))
+        return list
+    else:
+        raise ValueError(f'{path} does not exist')
+    
+
 
 
     
