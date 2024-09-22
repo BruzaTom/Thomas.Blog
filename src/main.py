3485895f2bdc40b109e606c976c51e59d4af6be9
BruzaTom <thomasagomez77@gmail.com>
2024-09-22 08:19:32 -0500

    upgrade to recusrive

--- src/main.py
+++ src/main.py
@@ -9,6 +9,7 @@ def main():
     #print({'public': get_dir(path)},'\n')
     #print(get_list(path))
     
+    #generate_page('content/index.md', 'template.html', 'public/index.html')
     generate_pages_recursive('content', 'template.html', 'public')
 
 def updateData(text, file):
@@ -16,44 +17,31 @@ def updateData(text, file):
         f.write(text)
 
 def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
-    tree = get_dir(dir_path_content)
-    files = []
-    for file in get_list(dir_path_content):
-        files.extend(file)
-    md_files = []
-    for file in files:
-        if file[len(file)-2:] == 'md':
-            md_files.append(file)
-    for file in md_files:
-        new_file_path = file.replace('md', 'html')
-        new_file_path = f'{dest_dir_path}/' + '/'.join(new_file_path.split('/')[1:])
-        print(new_file_path)
-        generate_page(file, template_path, new_file_path)
-
-
-
-def generate_page(from_path, template_path, dest_path):
-    print(f'generating page from {from_path}, to {dest_path}, using {template_path}')
     template = getText(template_path)
-    md = getText(from_path)
-    title = extract_title(from_path)
-    html = markdown_to_html_node(md)
-    htmlstring = html.to_html()
-    html_page = template.replace('{{ Title }}', title)
-    html_page = html_page.replace('{{ Content }}', htmlstring)
-    tree = get_dir('static')
-    dir_lst = dest_path.split('/')[1:]
+    content_tree = get_dir(dir_path_content)
+    static_tree = get_dir('static')
+    static_tree.update(content_tree)
+    static_files = get_list('static')
+    content_files = get_list(dir_path_content)
+    static_files.extend(content_files)
     files = []
-    for file in get_list('static'):
+    for file in static_files:
         files.extend(file)
-    for i in range(0, len(dir_lst)):
-        if '.' not in dir_lst[i]:
-            dict = {}
-            dict[dir_lst[i+1]] = None
-            tree[dir_lst[i]] = dict
-            files.append(f'{from_path.split("/")[0]}/' + '/'.join(dest_path.split('/')[1:]))
-    update_dir(dest_path.split('/')[0], tree, files)
-    updateData(html_page, dest_path)
+    
+    update_dir(dest_dir_path, static_tree, files)
+    content_paths = []
+    for file in content_files:
+        content_paths.extend(file)
+    for i in range(0, len(content_paths)):
+        template = getText(template_path)
+        md = getText(content_paths[i])
+        title = extract_title(content_paths[i])
+        html = markdown_to_html_node(md)
+        htmlstring = html.to_html()
+        html_page = getText(template_path).replace('{{ Title }}', title)
+        html_page = html_page.replace('{{ Content }}', htmlstring)
+        path = content_paths[i].replace('md', 'html')
+        updateData(html_page, path.replace(dir_path_content, dest_dir_path))
 
 def getText(path):
     with open(path) as f:
@@ -77,6 +65,10 @@ def update_dir(path, tree, files):
         if tree[node] == None:
             if (t+1) > len(files):
                 shutil.copy2('md.md', os.path.join(path, node))
+            if node[len(node)-2:] == 'md':
+                shutil.copy2(files[t], os.path.join(path, node.replace('md', 'html')))
+                t += 1
+                files = files[t:]
             else:
                 shutil.copy2(files[t], os.path.join(path, node))
                 t += 1
